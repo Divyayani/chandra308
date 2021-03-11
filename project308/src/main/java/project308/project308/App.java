cc7ee49badb4a8441ca934341530187cb2d70624
Ananya <Ananya@Ananya-PC>
2021-03-11 14:06:05 +0530

    16308

---
+++ project308/src/main/java/project308/project308/App.java
@@ -0,0 +1,13 @@
+package project308.project308;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
